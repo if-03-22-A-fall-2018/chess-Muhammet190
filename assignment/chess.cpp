e343de7a3468f20b6693a1f06c05d64cff552619
Muhammet190 <superman.batboy@outlook.com>
2018-11-12 13:32:06 +0100

    dsa

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -14,20 +14,62 @@
 #include "general.h"
 #include "chess.h"
 
-bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type){
+bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type)
+{
+  if (color == pc.color && type == pc.type)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+void 	init_chess_board (ChessBoard chess_board)
+{
+  for (int i = 0; i < 8; i++)
+  {
+		for (int j = 0; j < 8; j++)
+    {
+			chess_board[i][j].is_occupied = false;
+		}
+	}
 
 }
-void 	init_chess_board (ChessBoard chess_board){
+struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank)
+{
+ if (rank < 1 || rank > 8 || file < 'a' || file > 'h')
+ {
+   return 0;
+ }
+ else
+ {
+   return &chess_board[rank - 1][file - 'a'];
+ }
+}
+bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank)
+{
+  if (chess_board[rank -1 ][file -'a'].is_occupied == true)
+  {
+      return true;
+  }
+  else
+  {
+    return false;
+  }
 
 }
-struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank){
-
-}
-bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank){
-
-}
-bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece){
-
+bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece)
+{
+     if (rank < 1 || rank > 8 || file < 'a' || file > 'h' || is_square_occupied(chess_board,file,rank))
+     {
+          return false;
+     }
+     else
+     {
+      chess_board[rank -1][file -'a'].piece = piece;
+      return true;
+     }
 }
 struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank){
 
@@ -57,5 +99,5 @@ bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
 
 }
 bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-  
+
 }
